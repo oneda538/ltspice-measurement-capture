5d9c7b359b9669c34a7c74a57dc11e3801704b0b
Daniel O'Neill <oneda538@gmail.com>
2015-08-03 15:59:45 +1200

    If a measurement failed then ignored it. Make xlsx creation work

--- LT_meas_to_csv.py
+++ LT_meas_to_csv.py
@@ -1,4 +1,4 @@
-import time
+import time, sys, traceback
 from xlsxwriter.workbook import Workbook
 
 def code(inFileName):
@@ -34,7 +34,7 @@ def code(inFileName):
   #find measurement data
   measurementStartLines = []
   for i in range(len(lines)):
-      if "Measurement" in lines[i]:
+      if "Measurement" in lines[i] and "FAIL'ed" not in lines[i]:
           measurementStartLines.append(i)
 
   #add headers header column
@@ -45,6 +45,7 @@ def code(inFileName):
           
   print dataList[:20]
   dataToCSV(dataList, outFileName)
+  dataToXLSX(dataList, outFileName)
   
   
 def dataToCSV(dataList, outFileName):
@@ -62,37 +63,45 @@ def dataToXLSX(dataList, outFileName):
   workbook = Workbook(outFileName+".xlsx")
   worksheetData = workbook.add_worksheet('Data')
   
+  columnMap = {0:"A", 1:"B", 2:"C", 3:"D", 4:"E", 5:"F", 6:"G", 7:"H", 8:"I", 9:"J", 10:"K"}
   bold = workbook.add_format({'bold': 1})
 
   # Format the worksheet1 data cells that the chart will refer to
-  headings = ['Time', 'Set Temp(C)', 'Actual Temp(C)', 'Set Vin(V)', 'Actual Vin(V)', 'Iin(A)', 'Vout(V)', 'Set Iout(A)', 
-              'Actual Iout(A)', 'Vout AC(Vpp)', 'Vout AC(Vrms)', 'Efficiency', 'SpurFreq<200kHz(Hz)', 
-              'SpurLevel<200kHz(dB)', 'SpurFreq340kHz(Hz)', 'SpurLevel340kHz(dB)', 'L1 Temperature(degC)']
+  headings = dataList[0]
   
   # Set number format "constructor"
-  formatcells1 = workbook.add_format()
-  formatcells2 = workbook.add_format()
+  #formatcells1 = workbook.add_format()
+  #formatcells2 = workbook.add_format()
   
   # Format to 3 decimal places
-  formatcells1.set_num_format('0.000')
+  #formatcells1.set_num_format('0.000')
   
   # Columns A to F width set to 25. Note that it is not possible to "auto-fit" as this only happens at run-time in Excel
-  worksheetData.set_column('A:A', 25)
-  worksheetData.set_column('B:Q', 18)
+  worksheetData.set_column('A:%s' % columnMap[len(dataList[0])], 15)
+  #worksheetData.set_column('B:Q', 18)
   
   # Set horizontal and vertical alignment
-  formatcells1.set_align('center') 
-  formatcells1.set_align('vcenter')
-  formatcells2.set_align('center') 
-  formatcells2.set_align('vcenter')
+  #formatcells1.set_align('center') 
+  #formatcells1.set_align('vcenter')
+  #formatcells2.set_align('center') 
+  #formatcells2.set_align('vcenter')
   
   # Write headings row to workbook
   worksheetData.write_row('A1', headings, bold) # note that alignment not applicable to the write_row method
   
   
   ####repeat lots
-  worksheetData.write('D%i' % (row_count), float(configuredInputVolts), formatcells1)
-  self.worksheetData.write('A%i' % (self.row_count), time.ctime(), self.formatcells2)
+  rowCount = 2
+  for data in dataList[1:]:
+    columnCount = 0
+    for item in data:
+      print columnMap[columnCount], rowCount
+      worksheetData.write('%s%i' % (columnMap[columnCount], rowCount), float(item))
+      columnCount += 1
+    rowCount += 1
+      
+    #worksheetData.write('D%i' % (rowCount + 1), float(configuredInputVolts), formatcells1)
+    #self.worksheetData.write('A%i' % (rowCount), time.ctime(), self.formatcells2)
   
   workbook.close()
 
@@ -103,6 +112,9 @@ if __name__ == "__main__":
     code(inFileName)
   except:
     print "Fail"
+    ex_type, ex, tb = sys.exc_info()
+    traceback.print_tb(tb)
+    print ex_type, ex
   finally:
     print "Done"
     time.sleep(2)
